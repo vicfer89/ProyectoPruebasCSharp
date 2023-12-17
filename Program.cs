c07076e5bdb5480fc007d10c13b64e0ca54843dd
Víctor <vicfer89@gmail.com>
2023-12-17 18:38:30 +0100

    Programa de prueba para aprendizaje de C#
    Primera revisión para aprendizaje.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace consoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo desde VS Code");
+
+            Console.ReadKey();
+        }
+    }
+}
